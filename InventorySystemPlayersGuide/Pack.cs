6ef89aab47f386398ab47c5131a5589623e3bad0
Max360 <blythe2106@gnail.com>
2025-12-24 21:40:20 +0800

    Added ShowItemDetails() in the Pack class that shows each Item's details.

--- InventorySystemPlayersGuide/Pack.cs
+++ InventorySystemPlayersGuide/Pack.cs
@@ -2,23 +2,23 @@
 
 public class Pack
 {
-    public int TotalItem { get; init; }
+    public int MaximumItemCount { get; init; }
     public InventoryItem[] Items { get; set; }
     public float MaximumPackWeight { get; init; }
     public float MaximumPackVolume { get; init; }
     private int _itemCount = 0;
 
-    public Pack(int totalItem, float maximumPackWeight, float maximumPackVolume, InventoryItem[] Item = null)
+    public Pack(int maximumItemCount, float maximumPackWeight, float maximumPackVolume)
     {
-        TotalItem = totalItem;
+        MaximumItemCount = maximumItemCount;
         MaximumPackWeight = maximumPackWeight;
         MaximumPackVolume = maximumPackVolume;
-        Items = new InventoryItem[TotalItem];
+        Items = new InventoryItem[MaximumItemCount];
     }
 
     public bool Add(InventoryItem item)
     {
-        if (Items.Length > TotalItem)
+        if (Items.Length > MaximumItemCount)
         {
             Console.WriteLine($"ArrayExceededError! Cannot add more items in the Array! Current Item Count: {Items.Length}");
             return false;
@@ -41,4 +41,19 @@ public class Pack
         Console.WriteLine("Item added successfully!");
         return true;
     }
+
+    public void ShowItemDetails()
+    {
+        Console.WriteLine($"Maximum Item Count: {MaximumItemCount}");
+        Console.WriteLine($"Maximum Pack Weight: {MaximumPackWeight}");
+        Console.WriteLine($"Maximum Pack Volume: {MaximumPackVolume}");
+        for (int i = 0; i < Items.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}). Type: {Items[i].GetType().Name} Weight: {Items[i].Weight}, Volume: {Items[i].Volume}");
+        }
+    }
+
+    public static Pack SmallPack() => new Pack(10, 10.0f, 8.0f);
+    public static Pack MediumPack() => new Pack(15, 15.0f, 12.0f);
+    public static Pack LargePack() => new Pack(20, 20.0f, 15.0f);
 }
